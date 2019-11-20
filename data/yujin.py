8f831bffedc7b8f877f133f0c926ba7fd3b9e2f4
kim hyeong seop <gudtjqdl789@kaist.ac.kr>
2019-11-20 23:06:28 +0900

    data 정리

--- data/yujin.py
+++
@@ -1,5 +0,0 @@
-w = open('yujin.txt', 'w', encoding='UTF8')
-with open('유진데이터 텍스트파일.txt', 'rt', encoding='UTF8') as f:
-	for s in f:
-		#print(s[:-3]+'\t'+s[-2:-1])
-		w.write(s[:-3]+'\t'+s[-2:-1]+'\n')
